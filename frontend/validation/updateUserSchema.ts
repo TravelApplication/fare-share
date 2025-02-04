ba52eb2bf2f4c8d1a995e17b5eec53fd12a86fbc
Agata <agata.bartczak01@gmail.com>
2025-02-04 14:10:45 +0100

    add zod for validating User

--- frontend/validation/updateUserSchema.ts
+++ frontend/validation/updateUserSchema.ts
@@ -1,18 +1,30 @@
 import { z } from "zod";
 
 export const updateEmailSchema = z.object({
-    currentPassword: z
+    currentEmail: z
         .string()
-        .min(8, "Password must be at least 8 characters long"),
+        .nonempty("e-mail is required")
+        .email("Invalid email format"),
     newEmail: z
-        .string()
-        .email("Invalid email format")
-        .nonempty("Email is required"),
+        .string({
+            required_error: "New e-mail is required",
+        })
+        .email("Invalid e-mail format"),
+    currentPassword: z
+        .string({
+            required_error: "Password is required",
+        })
+        .nonempty("Current password is required")
+        .min(8, "Password must be at least 8 characters long"),
 });
 
 export const updatePasswordSchema = z.object({
     currentPassword: z
-        .string()
+        .string({
+            required_error: "Current password is required",
+        })
         .min(8, "Password must be at least 8 characters long"),
-    newPassword: z.string().min(8, "Password must be at least 8 characters long"),
+    newPassword: z.string({
+        required_error: "New password is required",
+    }).min(8, "Password must be at least 8 characters long"),
 });

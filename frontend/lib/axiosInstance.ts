0e102506b827521eb288286efafda86b52526265
Sebastian Jabłoński <sebaa1080@gmail.com>
2025-06-24 21:45:44 +0200

    Dodano interceptor przekierowujący na strone glowna przy requescie z konfliktem

--- frontend/lib/axiosInstance.ts
+++ frontend/lib/axiosInstance.ts
@@ -20,4 +20,23 @@ axiosInstance.interceptors.request.use(
   },
 );
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log('AXIOS INTERCEPTOR RESPONSE:', response.status);
+    return response;
+  },
+  (error) => {
+    console.log('AXIOS INTERCEPTOR ERROR:', error.response?.status);
+    if (
+      error.response &&
+      (error.response.status === 403 || error.response.status === 409)
+    ) {
+      if (typeof window !== 'undefined') {
+        window.location.href = '/trips';
+      }
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
